cb4bbb2f6e4222a26adbd55d628969e108fbbac6
Chris Closser <chrisclosser1@gmail.com>
2018-03-23 17:37:26 -0700

    playing with strings with nice prompt and striping first and last characters

--- src/str_play.c
+++ src/str_play.c
@@ -2,39 +2,74 @@
 #include <string.h>
 #include <stdlib.h>
 
-// char* remove_char(char* src)
-// {
-//   int len = strlen(src);
-//   printf("%s has a length of %i chars\n", src, len);
-//   char first = src[0];
-//   char c = first;
-//   printf("first char: %c\n", first);
-//   char *p = src;
-//   p++[strlen(p)] = '\0';
-//   printf("%s\n", p);
-//   // change int to char
-//   // int i = 65;
-//   // char c = i;
-//   // printf("%c", c); //prints A
-// }
-
 char* remove_char(char* dst, const char* src)
 {
   char *p = (char*)src;
-  p++[strlen(p)] = '\0';
-  printf("%s\n", p);
+  p++[strlen(p) - 1] = '\0';
+  // printf("%s\n", p);
   return (char*)p;
 }
 
+void flush();
 int main()
 {
-  char* buffer;
-  char word[] = "something";
-  const size_t buffer_size = strlen(word) + 1;
-  buffer = (char*) malloc(buffer_size);
-  remove_char(buffer, word);
-  free(buffer);
+    char str[25];
+    char hld[25];
+    // char word;
+
+    printf("Enter a word or sentence\n");
+    fgets(hld, 25, stdin);
+
+    printf("\nYou typed: %s\n", hld);
+    printf("Would you like to strip the first and last characters?\n");
+    fgets(str, 25, stdin);
+    // printf("hold %s\n", hld);
+    if (str[0]=='Y')
+      printf("you pressed yes or: %s\n", str);
+      char* buffer;
+      const size_t buffer_size = strlen(hld) + 1;
+      buffer = (char*) malloc(buffer_size);
+      printf("%s\n", remove_char(buffer, hld));
+      free(buffer);
+
+      // flush leaves the prompt running
+      // flush();
+
+    return 0;
 }
+
+void flush()
+{
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF)
+        ;
+}
+
+// int main () {
+//     char word;
+//
+//     printf("Type in a word \n");
+//     scanf("%s", word);
+//     printf("The word you typed was %s\n", word);
+// }
+
+// int main()
+// {
+//   char* buffer;
+//   // char word[1024];
+//   // scanf("Enter string to remove first and last character: %1023s", word);      /* scan until whitespace or no more space */
+//   // scanf("%1023[^\n]", word);  /* scan until newline or no more space */
+//   // fgets(word, 1024, stdin);   /* scan including newline or no more space */
+//   // printf("Enter string to remove first and last character: ");
+//   // fgets(word);
+//   // printf("You said: %s\n", word);
+//   // char x[10];
+//   char word[1024] = raw_input("Enter string to remove first and last character: ", word, 1024);
+//   const size_t buffer_size = strlen(word) + 1;
+//   buffer = (char*) malloc(buffer_size);
+//   remove_char(buffer, word);
+//   free(buffer);
+// }
 // #include <criterion>
 // #include <string.h>
 // #include <stdlib.h>
