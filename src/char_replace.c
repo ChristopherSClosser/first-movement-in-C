03d7a3afb74573070cab01189d312ed36679be81
Chris Closser <chrisclosser1@gmail.com>
2018-03-26 11:41:25 -0700

    clean

--- src/char_replace.c
+++ src/char_replace.c
@@ -2,17 +2,12 @@
 #include <string.h>
 #include <stdlib.h>
 
-// char *correct(char *string)
-// {
-//
-// }
-
 char* replacechar(char *str, char *orig, char *rep) {
-    char *ix = str;
-    int n = 0;
-    while((ix = strchr(ix, *orig)) != NULL) {
-        *ix++ = *rep;
-        n++;
+    char *chr = str;
+    int i = 0;
+    while((chr = strchr(chr, *orig)) != NULL) {
+        *chr++ = *rep;
+        i++;
     }
     return (char*)str;
 }
@@ -22,7 +17,6 @@ int main() {
   char hld[25];
   char orig[25];
   char rep[25];
-  // char word;
 
   printf("Enter a word or sentence\n");
   fgets(hld, 25, stdin);
@@ -30,7 +24,7 @@ int main() {
   printf("\nYou typed: %s\n", hld);
   printf("Would you like to replace any characters?\n");
   fgets(str, 25, stdin);
-  // printf("hold %s\n", hld);
+
   if (str[0]=='Y')
   {
     printf("Enter character to replace:\n");
@@ -38,13 +32,7 @@ int main() {
     printf("you entered: %s\nEnter replacement character:\n", orig);
     fgets(rep, 25, stdin);
     printf("you entered: %s\n", rep);
-    // printf("Enter replacement character:\n");
-    // fgets(rep, 25, stdin);
-    // char* buffer;
-    // const size_t buffer_size = strlen(hld) + 1;
-    // buffer = (char*) malloc(buffer_size);
     printf("%s\n", replacechar(hld, orig, rep));
-    // free(buffer);
 
     /* flush leaves the prompt running */
     // flush();
