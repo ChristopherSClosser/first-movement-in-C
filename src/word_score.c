f447ad8fb4afb556fd21628f0ddea118ca07b56d
Chris Closser <chrisclosser1@gmail.com>
2018-03-28 19:47:00 -0700

    trying

--- src/word_score.c
+++ src/word_score.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/*
 """
 ------------- Python version ---------------
 
@@ -12,3 +16,21 @@ def high(x):
         scores.append(total)
     return wlist[scores.index(max(scores))]
 """
+
+ @param str: a C-string containing only lowercase letters and spaces (' ')
+    @return: a C-string allocated on the heap containing the highest scoring word of str
+*/
+char *highestScoringWord(char *str)
+{
+  char *p = str;
+  const char *alph = "abcdefghijklmnopqrstuvwxyz";
+  char * found = strstr( alph, "z" );
+  int idx = found - alph + 1;
+  printf("%i\n", idx);
+  return p;
+}
+
+int main(int argc, char const *argv[]) {
+  printf("%s\n", highestScoringWord("OnE"));
+  return 0;
+}
